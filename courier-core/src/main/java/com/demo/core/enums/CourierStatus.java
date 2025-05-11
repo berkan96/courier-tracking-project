fdae629e56bfc28a1f06e98a17faa3cc3f8d92eb
berkan96 <berkanakbal96@gmail.com>
2025-05-11 23:14:29 +0300

    fix.

---
+++ courier-core/src/main/java/com/demo/core/enums/CourierStatus.java
@@ -0,0 +1,7 @@
+package com.demo.core.enums;
+
+public enum CourierStatus {
+    AVAILABLE,
+    ON_DELIVERY,
+    NOT_WORKING
+}
---
+++ courier-core/src/main/java/com/demo/core/event/CourierLocationEvent.java
@@ -0,0 +1,23 @@
+package com.demo.core.event;
+
+import com.demo.core.enums.CourierStatus;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CourierLocationEvent implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long courierId;
+    private Double latitude;
+    private Double longitude;
+    private CourierStatus status;
+    private LocalDateTime recordedAt;
+}
